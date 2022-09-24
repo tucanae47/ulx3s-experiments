6d6cffc3feea6ce8bf9f8adf5e13927c65e8fa06
tucanae47 <cmsvalenzuela@gmail.com>
2022-09-24 17:36:35 +0200

    enable lower scale mode, register uddate, 160x120

--- cam/top_ov7670.v
+++ cam/top_ov7670.v
@@ -30,17 +30,17 @@ module top_ov7670
       // c_img_pxls    = c_img_cols * c_img_rows,
       // c_nb_line_pxls = 9, // log2i(c_img_cols-1) + 1;
       // c_nb_img_pxls =  17,  //320*240=76,800 -> 2^17
-      c_img_cols    = 160, // 8 bits
-      c_img_rows    = 120, //  7 bits
-      c_nb_line_pxls = 8, // log2i(c_img_cols-1) + 1;
-      c_img_pxls    = c_img_cols * c_img_rows,
-      c_nb_img_pxls =  15,  //160*120=19.200 -> 2^15
-      // QQVGA
-      // c_img_cols    = 120, // 8 bits
-      // c_img_rows    = 90, //  7 bits
-      // c_nb_line_pxls = 7, // log2i(c_img_cols-1) + 1;
+      // c_img_cols    = 160, // 8 bits
+      // c_img_rows    = 120, //  7 bits
+      // c_nb_line_pxls = 8, // log2i(c_img_cols-1) + 1;
       // c_img_pxls    = c_img_cols * c_img_rows,
-      // c_nb_img_pxls =  13,  //160*120=19.200 -> 2^15
+      // c_nb_img_pxls =  15,  //160*120=19.200 -> 2^15
+      // QQVGA
+      c_img_cols    = 120, // 8 bits
+      c_img_rows    = 90, //  7 bits
+      c_nb_line_pxls = 7, // log2i(c_img_cols-1) + 1;
+      c_img_pxls    = c_img_cols * c_img_rows,
+      c_nb_img_pxls =  14,  //160*120=19.200 -> 2^15
       // QQVGA /2
       //c_img_cols    = 80, // 7 bits
       //c_img_rows    = 60, //  6 bits

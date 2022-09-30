42cee91e32c9e09e51e87f43c36c1ae9ecbef632
tucanae47 <cmsvalenzuela@gmail.com>
2022-09-30 13:31:42 +0200

    Testo Fifo again

--- cam/top_ov7670.v
+++ cam/top_ov7670.v
@@ -30,11 +30,11 @@ module top_ov7670
      // c_img_pxls    = c_img_cols * c_img_rows,
      // c_nb_line_pxls = 9, // log2i(c_img_cols-1) + 1;
      // c_nb_img_pxls =  17,  //320*240=76,800 -> 2^17
-     c_img_cols    = 160, // 8 bits
-     c_img_rows    = 120, //  7 bits
-     c_nb_line_pxls = 8, // log2i(c_img_cols-1) + 1;
-     c_img_pxls    = c_img_cols * c_img_rows,
-     c_nb_img_pxls =  15,  //160*120=19.200 -> 2^15
+    //  c_img_cols    = 160, // 8 bits
+    //  c_img_rows    = 120, //  7 bits
+    //  c_nb_line_pxls = 8, // log2i(c_img_cols-1) + 1;
+    //  c_img_pxls    = c_img_cols * c_img_rows,
+    //  c_nb_img_pxls =  15,  //160*120=19.200 -> 2^15
      // QQVGA
      // c_img_cols    = 120, // 8 bits
      // c_img_rows    = 90, //  7 bits
@@ -42,11 +42,11 @@ module top_ov7670
      // c_img_pxls    = c_img_cols * c_img_rows,
      // c_nb_img_pxls =  14,  //160*120=19.200 -> 2^15
      // QQVGA /2
-    //  c_nb_line_pxls = 7, // log2i(c_img_cols-1) + 1;
-    //  c_img_cols    = 80, // 7 bits
-    //  c_img_rows    = 60, //  6 bits
-    //  c_img_pxls    = c_img_cols * c_img_rows,
-    //  c_nb_img_pxls =  13,  //80*60=4800 -> 2^13
+     c_nb_line_pxls = 7, // log2i(c_img_cols-1) + 1;
+     c_img_cols    = 80, // 7 bits
+     c_img_rows    = 60, //  6 bits
+     c_img_pxls    = c_img_cols * c_img_rows,
+     c_nb_img_pxls =  13,  //80*60=4800 -> 2^13
 
      c_nb_buf_red   =  4,  // n bits for red in the buffer (memory)
      c_nb_buf_green =  4,  // n bits for green in the buffer (memory)
@@ -149,99 +149,99 @@ module top_ov7670
 
 
 
-  vga_sync i_vga
-           (
-             .rst     (rst),
-             .clk     (wclk),
-             .visible (vga_visible),
-             .new_pxl (vga_new_pxl),
-             .hsync   (vga_hsync_wr),
-             .vsync   (vga_vsync_wr),
-             .col     (vga_col),
-             .row     (vga_row)
-           );
+  // vga_sync i_vga
+  //          (
+  //            .rst     (rst),
+  //            .clk     (wclk),
+  //            .visible (vga_visible),
+  //            .new_pxl (vga_new_pxl),
+  //            .hsync   (vga_hsync_wr),
+  //            .vsync   (vga_vsync_wr),
+  //            .col     (vga_col),
+  //            .row     (vga_row)
+  //          );
 
 
-    vga_display
-    # (
-        .c_img_cols(c_img_cols), // 7 bits
-        .c_img_rows(c_img_rows), //  6 bits
-        .c_img_pxls(c_img_cols * c_img_rows),
-        .c_nb_img_pxls(c_nb_img_pxls)
-    )
-    I_ov_display
-    (
-       .rst        (rst),
-       .clk        (wclk),
-       .visible    (vga_visible),
-       .new_pxl    (vga_new_pxl),
-       .hsync      (vga_hsync_wr),
-       .vsync      (vga_vsync_wr),
-       .rgbmode    (rgbmode),
-       .col        (vga_col),
-       .row        (vga_row),
-       .frame_pixel(orig_img_pxl),
-       .frame_addr (orig_img_addr),
-       .hsync_out  (vga_hsync),
-       .vsync_out  (vga_vsync),
-       .vga_red    (vga_red),
-       .vga_green  (vga_green),
-       .vga_blue   (vga_blue)
-    );
+  //   vga_display
+  //   # (
+  //       .c_img_cols(c_img_cols), // 7 bits
+  //       .c_img_rows(c_img_rows), //  6 bits
+  //       .c_img_pxls(c_img_cols * c_img_rows),
+  //       .c_nb_img_pxls(c_nb_img_pxls)
+  //   )
+  //   I_ov_display
+  //   (
+  //      .rst        (rst),
+  //      .clk        (wclk),
+  //      .visible    (vga_visible),
+  //      .new_pxl    (vga_new_pxl),
+  //      .hsync      (vga_hsync_wr),
+  //      .vsync      (vga_vsync_wr),
+  //      .rgbmode    (rgbmode),
+  //      .col        (vga_col),
+  //      .row        (vga_row),
+  //      .frame_pixel(orig_img_pxl),
+  //      .frame_addr (orig_img_addr),
+  //      .hsync_out  (vga_hsync),
+  //      .vsync_out  (vga_vsync),
+  //      .vga_red    (vga_red),
+  //      .vga_green  (vga_green),
+  //      .vga_blue   (vga_blue)
+  //   );
   // count 2 clock cycles to get a pixel cycle
-  // reg            cnt_clk; // count 0 to 1: 2 clk cycles, from 50MHz to 25MHz
-  // reg  [10-1:0]  cnt_pxl;
-  // reg  [10-1:0]  cnt_line;
+  reg            cnt_clk; // count 0 to 1: 2 clk cycles, from 50MHz to 25MHz
+  reg  [10-1:0]  cnt_pxl;
+  reg  [10-1:0]  cnt_line;
 
-  // wire   end_cnt_pxl;
-  // wire   end_cnt_line;
-  // wire   new_line, new_pxl;
-  // always @ (posedge rclk)
-  // begin
-  //   if (rst)
-  //     cnt_clk <= 1'b0;
-  //   else
-  //     cnt_clk <= ~cnt_clk;
-  // end
+  wire   end_cnt_pxl;
+  wire   end_cnt_line;
+  wire   new_line, new_pxl;
+  always @ (posedge rclk)
+  begin
+    if (rst)
+      cnt_clk <= 1'b0;
+    else
+      cnt_clk <= ~cnt_clk;
+  end
 
-  // assign new_pxl =  cnt_clk;
-  // always @ (posedge rclk)
-  // begin
-  //   if (rst)
-  //     orig_img_addr <= 0;
-  //   else
-  //   begin
-  //     if (vga_row < c_img_rows)
-  //     begin
-  //       if (vga_col < c_img_cols)
-  //       begin
-  //         if (new_pxl)
-  //           //it may have a simulation problem in the last pixel of the last row
-  //           orig_img_addr <= orig_img_addr + 1;
-  //       end
-  //     end
-  //     else
-  //       orig_img_addr <= 0;
-  //   end
-  // end
+  assign new_pxl =  cnt_clk;
+  always @ (posedge rclk)
+  begin
+    if (rst)
+      orig_img_addr <= 0;
+    else
+    begin
+      if (vga_row < c_img_rows)
+      begin
+        if (vga_col < c_img_cols)
+        begin
+          if (new_pxl)
+            //it may have a simulation problem in the last pixel of the last row
+            orig_img_addr <= orig_img_addr + 1;
+        end
+      end
+      else
+        orig_img_addr <= 0;
+    end
+  end
 
   // camera frame buffer, before processing
-  frame_buffer
-    # (
-      .c_img_cols(c_img_cols), // 7 bits
-      .c_img_rows(c_img_rows), //  6 bits
-      .c_img_pxls(c_img_cols * c_img_rows),
-      .c_nb_img_pxls(c_nb_img_pxls)
-    )
-    cam_fb
-    (
-      .clk     (wclk),
-      .wea     (capture_wen),
-      .addra   (capture_addr),
-      .dina    (capture_data),
-      .addrb   (orig_img_addr),
-      .doutb   (orig_img_pxl)
-    );
+  // frame_buffer
+  //   # (
+  //     .c_img_cols(c_img_cols), // 7 bits
+  //     .c_img_rows(c_img_rows), //  6 bits
+  //     .c_img_pxls(c_img_cols * c_img_rows),
+  //     .c_nb_img_pxls(c_nb_img_pxls)
+  //   )
+  //   cam_fb
+  //   (
+  //     .clk     (rclk),
+  //     .wea     (capture_wen),
+  //     .addra   (capture_addr),
+  //     .dina    (capture_data),
+  //     .addrb   (orig_img_addr),
+  //     .doutb   (orig_img_pxl)
+  //   );
 
 
   // stop capturing to see what happens with the image
@@ -314,11 +314,12 @@ module top_ov7670
   //  wire [15:0] color = x[3] ^ y[3] ? {5'd0, x[6:1], 5'd0} : {y[5:1], 6'd0, 5'd0};
   //  wire [15:0] color = color_pxl;
   //  wire [15:0] color = {g, b, r};
-  // wire [7:0] color = {b, g, r};
+  wire [7:0] color = {r,g, b};
 
-  reg [8:0] color;
+  // reg [8:0] color;
   // wire [15:0] color;
   wire next_pixel;
+  wire oclk;
 
   oled_video
     #(
@@ -329,7 +330,7 @@ module top_ov7670
     )
     oled_video_inst
     (
-      .clk(rclk),
+      .clk(oclk),
       .reset(~btn0),
       .next_pixel(next_pixel),
       .x(x),
@@ -342,109 +343,110 @@ module top_ov7670
       .spi_resn(oled_resn)
     );
   
-    always @(posedge rclk) begin
+  //   always @(posedge rclk) begin
+  //     if (next_pixel) begin
+  //     r  <= capture_data[c_nb_buf-1: c_nb_buf-c_nb_buf_red];
+  //     g  <= capture_data[c_nb_buf-c_nb_buf_red-1:c_nb_buf_blue];
+  //     b  <= capture_data[c_nb_buf_blue-1:0]; 
+  //     // color<= {r[3:0], g[3:0], b[2:0]};
+
+  //   end
+  // end
+
+
+  assign wclk = clk50mhz;
+  // assign wclk = clk25mhz;
+  assign oclk = clk25mhz;
+  // //   assign rrst_n = btn0;
+
+
+  always @(posedge wclk)
+  begin
+    if(rst)
+    begin
+      winc<= 1;
+      wrst_n <= 0;
+    end
+    else
+    begin
+      //  wdata <= {{1'b0, vga_red},{1'b0, vga_green},{2'b0, vga_blue}};
+      // wdata <= orig_img_pxl;
+      wdata <= capture_data;
+    end
+  end
+
+  always @(posedge rclk)
+  begin
+    if(rst)
+    begin
+      rinc <=1;
+      rrst_n<=0;
+    end
+    else
+    begin
+
+      //   if ((x < c_img_cols) && (y < c_img_rows)) begin
+      // r  <= {1'b0, rdata[c_nb_buf-1: c_nb_buf-c_nb_buf_red]};
+      // g  <= {1'b0, rdata[c_nb_buf-c_nb_buf_red-1:c_nb_buf_blue]};
+      // b  <= {2'b0, rdata[c_nb_buf_blue-1:0]};
+
+      // r  <= rdata[c_nb_buf-1: c_nb_buf-c_nb_buf_red];
+      // g  <= rdata[c_nb_buf-c_nb_buf_red-1:c_nb_buf_blue];
+      // b  <= rdata[c_nb_buf_blue-1:0];
+
       if (next_pixel) begin
-      r  <= orig_img_pxl[c_nb_buf-1: c_nb_buf-c_nb_buf_red];
-      g  <= orig_img_pxl[c_nb_buf-c_nb_buf_red-1:c_nb_buf_blue];
-      b  <= orig_img_pxl[c_nb_buf_blue-1:0]; 
-      color<= {r[3:0], g[3:0], b[2:0]};
+        // if ((x < 128) && (y < 128)) begin
+
+      r  <= rdata[c_nb_buf-1: c_nb_buf-c_nb_buf_red];
+      g  <= rdata[c_nb_buf-c_nb_buf_red-1:c_nb_buf_blue];
+      b  <= rdata[c_nb_buf_blue-1:0];
+
+      // r  <= orig_img_pxl[c_nb_buf-1: c_nb_buf-c_nb_buf_red];
+      // g  <= orig_img_pxl[c_nb_buf-c_nb_buf_red-1:c_nb_buf_blue];
+      // b  <= orig_img_pxl[c_nb_buf_blue-1:0];
+      // r  <= {1'b0, rdata[c_nb_buf-1: c_nb_buf-c_nb_buf_red]};
+      // g  <= {1'b0, rdata[c_nb_buf-c_nb_buf_red-1:c_nb_buf_blue]};
+      // b  <= {2'b0, rdata[c_nb_buf_blue-1:0]};
+          // r  <= rdata[7:4];
+          // g  <= rdata[7:4];
+          // b  <= rdata[7:4];
+
+          // color <= {r[2:0], g[2:0], b[1:0]};
+        end
+      // end
+      //   end
+      //   else begin
+      //     r <=5'b0;
+      //     g <=5'b0;
+      //     b <=6'b0;
+      //   end
+
+      // color_pxl<=rdata;
 
     end
   end
 
-
-  assign wclk = clk50mhz;
-  assign rclk = clk25mhz;
-  // //   assign rrst_n = btn0;
-
-
-  // always @(posedge wclk)
-  // begin
-  //   if(rst)
-  //   begin
-  //     winc<= 1;
-  //     wrst_n <= 0;
-  //   end
-  //   else
-  //   begin
-  //     //  wdata <= {{1'b0, vga_red},{1'b0, vga_green},{2'b0, vga_blue}};
-  //     // wdata <= orig_img_pxl;
-  //     wdata <= capture_data;
-  //   end
-  // end
-
-  // always @(posedge rclk)
-  // begin
-  //   if(rst)
-  //   begin
-  //     rinc <=1;
-  //     rrst_n<=0;
-  //   end
-  //   else
-  //   begin
-
-  //     //   if ((x < c_img_cols) && (y < c_img_rows)) begin
-  //     // r  <= {1'b0, rdata[c_nb_buf-1: c_nb_buf-c_nb_buf_red]};
-  //     // g  <= {1'b0, rdata[c_nb_buf-c_nb_buf_red-1:c_nb_buf_blue]};
-  //     // b  <= {2'b0, rdata[c_nb_buf_blue-1:0]};
-
-  //     // r  <= rdata[c_nb_buf-1: c_nb_buf-c_nb_buf_red];
-  //     // g  <= rdata[c_nb_buf-c_nb_buf_red-1:c_nb_buf_blue];
-  //     // b  <= rdata[c_nb_buf_blue-1:0];
-
-  //     if (next_pixel) begin
-  //       // if ((x < 128) && (y < 128)) begin
-
-  //     // r  <= rdata[c_nb_buf-1: c_nb_buf-c_nb_buf_red];
-  //     // g  <= rdata[c_nb_buf-c_nb_buf_red-1:c_nb_buf_blue];
-  //     // b  <= rdata[c_nb_buf_blue-1:0];
-
-  //     r  <= orig_img_pxl[c_nb_buf-1: c_nb_buf-c_nb_buf_red];
-  //     g  <= orig_img_pxl[c_nb_buf-c_nb_buf_red-1:c_nb_buf_blue];
-  //     b  <= orig_img_pxl[c_nb_buf_blue-1:0];
-  //     // r  <= {1'b0, rdata[c_nb_buf-1: c_nb_buf-c_nb_buf_red]};
-  //     // g  <= {1'b0, rdata[c_nb_buf-c_nb_buf_red-1:c_nb_buf_blue]};
-  //     // b  <= {2'b0, rdata[c_nb_buf_blue-1:0]};
-  //         // r  <= rdata[7:4];
-  //         // g  <= rdata[7:4];
-  //         // b  <= rdata[7:4];
-
-  //         // color <= {r[2:0], g[2:0], b[1:0]};
-  //       end
-  //     // end
-  //     //   end
-  //     //   else begin
-  //     //     r <=5'b0;
-  //     //     g <=5'b0;
-  //     //     b <=6'b0;
-  //     //   end
-
-  //     // color_pxl<=rdata;
-
-  //   end
-  // end
-
   // we need async fifo to keep to clocks one for the tpu and other for the input stream
-  // async_fifo
-  //   #(
-  //     DSIZE,
-  //     ASIZE
-  //   )
-  //   fifo
-  //   (
-  //     wclk,
-  //     wrst_n,
-  //     winc,
-  //     wdata,
-  //     wfull,
-  //     awfull,
-  //     rclk,
-  //     rrst_n,
-  //     rinc,
-  //     rdata,
-  //     rempty,
-  //     arempty
-  //   );
+  async_fifo
+    #(
+      DSIZE,
+      ASIZE
+    )
+    fifo
+    (
+      wclk,
+      wrst_n,
+      winc,
+      wdata,
+      wfull,
+      awfull,
+      rclk,
+      rrst_n,
+      rinc,
+      rdata,
+      rempty,
+      arempty
+    );
 
 
 
